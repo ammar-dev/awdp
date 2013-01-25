60f88c034ee3a9263c78fad4fdeb73f8db066cb4
ammar <ammar@ammar-laptop.(none)>
2013-01-25 23:31:12 +0500

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depo::Application.config.secret_token = '267edbcbec9beab8a67a6fd97ff2cc213e2aef613e005cb42a3e59e41a910f40c10ef52b59e6bba3bdc5bc202190f792532652cc31ba78f88e225185e53d8df5'
