971015dfa3ea88a9aaeb0b58df599a236ac6aefa
ammar <ammar@ammar-laptop.(none)>
2013-01-27 17:39:14 +0500

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_i18n_locale_from_params
+  before_filter :authorize
 
   private
 
@@ -10,5 +12,38 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = cart.id
     cart
   end
+
+
+  protected
+
+  def authorize
+    if request.format == Mime::HTML
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    else
+      authenticate_or_request_with_http_basic do |username, password|
+        user = User.find_by_name(username)
+        user && user.authenticate(password)
+      end
+    end
+  end
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.include?(params[:locale].to_sym)
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] =
+            "#{params[:locale]} translation not available"
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
+
 
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,9 +1,14 @@
 class StoreController < ApplicationController
-  def index
-    @products = Product.order(:title)
-    @cart = current_cart
-  end
+  skip_before_filter :authorize
 
+  def index
+    if params[:set_locale]
+      redirect_to store_path(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+      @cart = current_cart
+    end
+  end
 
 
 end
